59afb24faedc48d00307840f1eb39bed177e60eb
okshi <johnstarhafiz@gmail.com>
2021-11-03 10:34:00 +0600

    Connect server with mongodb database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
@@ -6,8 +7,19 @@ const port = process.env.PORT || 5000;
 
 
 //MiddleWare
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+
+//Database String
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.luy9u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+
+
 
 app.get('/', (req, res) => {
      res.send('Hello Doctors Servers !')
